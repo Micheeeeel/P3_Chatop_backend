8d5fa654a999105cb6b94941f0c6e6dab1bf0d74
MONPCDECOMPET\Thomas MICHEL <michelt.thomas@gmail.com>
2023-05-23 16:31:11 +0200

    Implemented Spring Security configuration with role-based access control, in-memory user details service, and password encoding

--- src/main/java/com/chatop/backend/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/backend/configuration/SpringSecurityConfig.java
@@ -1,5 +1,46 @@
 package com.chatop.backend.configuration;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
 public class SpringSecurityConfig {
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests(authorizeRequests -> 
+            authorizeRequests
+            .antMatchers("/admin").hasRole("ADMIN")
+            .antMatchers("/user").hasRole("USER")
+            .anyRequest().authenticated()
+        )
+        .formLogin(formLogin -> {})
+        .oauth2Login(oauth2 -> {});
+
+    return http.build();
+    }
+    
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("springuser").password(passwordEncoder().encode("spring123")).roles("USER").build());
+        manager.createUser(User.withUsername("springadmin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER").build());
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
